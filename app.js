037324f4b0cf0211d5d393db1ff69c084f0f38b1
levananh212 <levananh212@gmail.com>
2017-07-16 12:17:14 +0700

    'routes'

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoskin = require('mongoskin');
+const db = mongoskin.db('mongodb://localhost:27017/st',{native_parser:true}); 
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+const products = require('./routes/products');
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port);
+
+app.set('view engine','ejs');
+app.set('views','views');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use((req,res,next) => {
+    req.db = {};
+    req.db.products = db.collection('products');
+    next();
+});
+
+app.get('/',routes.index);
+app.get('/about',routes.about);
+app.get('/contact',routes.contact);
+app.get('/products',products.allProducts);
+app.get('/products/:product_name',products.oneProduct);
+app.post('/products/:product_name',products.addProduct);
+app.delete('/products/:product_name',products.delProduct);
+
+app.all('*',(req,res) => res.status(404).send());
+app.use((err,req,res,next) => res.status(500).send(err.toString()));
---
+++ routes/products.js
@@ -0,0 +1,34 @@
+exports.allProducts = (req,res,next) => {
+    req.db.products.find().toArray((err,products) => {
+        if (err) return next(err);
+        res.render('products', {title : 'Products' , products });
+    });
+}
+
+exports.oneProduct = (req,res,next) => {
+    req.db.products.find({_id : req.product._id}).toArray((err,product) => {
+        if (err) return next(err);
+        res.render('product', {title : 'Product' , product });
+    });
+}
+
+exports.addProduct = (req,res,next) => {
+    if (!req.body || !req.body.name || !req.body.img) return next(new Error('No data provide'));
+    req.db.products.save({
+        name : req.body.name,
+        img : req.body.img,
+        desc : req.body.desc
+    }, (err,product) => {
+        if (err) return next(err);
+        if (!product) return next(new Error('Fail to add!'));
+        res.redirect('/');
+    });
+}
+
+exports.delProduct = (req,res,next) => {
+    req.db.products.removeById(req.product_id, (err,pcount) => {
+        if (err) return next(err);
+        if (count !== 1) return next(new Error('Fail to delete!'));
+        res.redirect('/');
+    });
+}
